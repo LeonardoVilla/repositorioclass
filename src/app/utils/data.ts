4befcad268c5234b6af95d2978224ecdad167025
LEONARDO <leosivi@gmail.com>
2025-10-31 16:46:26 -0400

    Atualizar link em utils

---
+++ src/app/utils/data.ts
@@ -0,0 +1,39 @@
+// utils/data.ts
+export const softwares = [
+  { name: "Composer", url: "https://getcomposer.org/download/" },
+  { name: "Git", url: "https://git-scm.com/install/windows" },
+  { name: "XAMPP", url: "https://sourceforge.net/projects/xampp/files/XAMPP%20Windows/8.2.12/" },
+  { name: "PHP", url: "https://windows.php.net/downloads/releases/archives/" },
+  { name: "Node.js", url: "https://nodejs.org/en/download" },
+  { name: "MariaDB", url: "https://mariadb.org/" },
+  { name: "MySQL", url: "https://www.oracle.com/mysql/technologies/mysql-enterprise-edition-downloads.html" },
+];
+
+export const servicos = [
+  { name: "Supabase", url: "https://supabase.com/" },
+  { name: "Firebase", url: "https://www.firebase.google.com/docs/auth?hl=pt-br" },
+  { name: "MongoDB", url: "https://www.mongodb.com/" },
+  { name: "React Expo", url: "https://docs.expo.dev/" },
+  { name: "Laravel", url: "https://laravel.com/" },
+  { name: "Carousel ", url: "https://swiperjs.com/demos/" },
+  { name: "Bootstrap ", url: "https://getbootstrap.com/" },
+  { name: "Reactnativepaper", url: "https://reactnativepaper.com/" },
+  { name: "Mapeador Json", url: "https://jsoncrack.com/editor" },
+  { name: "Postman", url: "https://identity.getpostman.com/" },
+];
+
+export const tutoriaisdev = [
+  { name: "W3schools", url: "https://www.w3schools.com/" },
+  { name: "Reactnative", url: "https://reactnativepaper.com/" },
+  { name: "Medium Tutoriais", url: "https://medium.com/@leonardovilla.tech/" },
+  { name: "Medium Laravel", url: "https://medium.com/@leonardovilla.tech/laravel-12-crud-com-livewire-988933f3773b" },
+];
+
+export const tutoriaisbd = [
+  { name: "DB Diagram", url: "https://dbdiagram.io/d" },
+  { name: "Runsql", url: "https://dbdiagram.io/" },
+  { name: "Hacker Rank SQL", url: "https://www.hackerrank.com/domains/sql/" },
+  { name: "LeetCode SQL", url: "https://leetcode.com/problemset/database/" },
+  { name: "SQLzoo", url: "https://sqlzoo.net/wiki/SQL_Tutorial" },
+  { name: "Mode SQL", url: "https://mode.com/sql-tutorial" },
+];
