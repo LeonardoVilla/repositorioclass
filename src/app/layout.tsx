ad425b041cccc8d3e5ea822b7fd405802290fb81
LEONARDO <leosivi@gmail.com>
2025-11-07 07:50:31 -0400

    fix: corrigir importações de Navbar e Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-// import Navbar from "@/app/components/layout/Navbar"
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
-// import Footer from "@/app/components/layout/Footer";
 import AdBanner from "@/app/components/adsense/GoogleAdBanner";
 import GoogleAdSenseMeta from "@/app/components/adsense/GoogleAdSenseMeta";
 import GoogleConsent from "@/app/components/scripts/adsense/GoogleConsent";
